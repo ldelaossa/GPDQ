79bfe3e41445f6ceb16aa4b6d354ea33c0924b81
Luis de la Ossa <luis.delaossa@uclm.es>
2019-03-25 22:00:39 +0100

    Deletes old version

--- gpdq/util/gui/colorString.m
+++
@@ -1,3 +0,0 @@
-function string = colorString(color, text)
-    string = ['<html><table border=0 width=200 bgcolor=', color,' ><TR><TD> <font color="black"><b>',text ,'</b></font></TD></TR></table></html>'];
-end
