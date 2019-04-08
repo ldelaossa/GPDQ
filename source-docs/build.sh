3c57c8a1fb941c5ff94ad8b72e13dce1f7ddc65a
Luis de la Ossa <luis.delaossa@uclm.es>
2019-04-08 22:24:10 +0200

    Deleted sources

--- source-docs/build.sh
+++
@@ -1,9 +0,0 @@
-make html
-sphinx-build -b rinoh . ../build/pdf
-rm -rf ../docs/*
-mkdir ../docs
-mv ../build/html ../docs
-mkdir ../docs/pdf
-mv ../build/pdf/*.pdf ../docs/pdf
-rm -rf ../build
-echo '<meta http-equiv="refresh" content="0; url=html/index.html" />' > ../docs/index.html
--- source-docs/buildhtml.sh
+++
@@ -1,3 +0,0 @@
-make html
-rm -rf ../docs/doctrees
-echo '<meta http-equiv="refresh" content="0; url=html/index.html" />' > ../docs/index.html
